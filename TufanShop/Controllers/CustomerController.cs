386672eb0f0f070ff86343ccb86d81ba3cce927f
Tufan Butuner <tufanbutuner@gmail.com>
2020-06-21 00:17:22 +0100

    Initial commit

---
+++ TufanShop/Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TufanShop.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void LoginUser()
+        {
+            Console.WriteLine("Hello Onik!");
+        }
+    }
+}
